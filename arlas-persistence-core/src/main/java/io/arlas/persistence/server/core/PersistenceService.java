a84e4f018328d684e2a710cb1182b6e8acf2f6ea
alainbodiguel <alain.bodiguel@gisaia.com>
2020-10-09 17:22:49 +0200

    Added group/public to group list

--- arlas-persistence-core/src/main/java/io/arlas/persistence/server/core/PersistenceService.java
+++ arlas-persistence-core/src/main/java/io/arlas/persistence/server/core/PersistenceService.java
@@ -69,7 +69,7 @@ public interface PersistenceService {
 
     static List<String> getGroupsForZone(String zone,
                                          IdentityParam identityParam) {
-        return identityParam.groups.stream().filter(g -> g.contains(zone)).collect(Collectors.toList());
+        return identityParam.groups.stream().filter(g -> g.contains(zone) || g.equals("group/public")).collect(Collectors.toList());
     }
 
     static boolean intersect(List<String> a, List<String> b) {
