f19d576ec0212badbe40de4ee499227aa0ce43a4
Mohamed Hamou <mohamed.hamou@gisaia.com>
2023-03-05 15:10:27 +0100

    development version 23.0.6-SNAPSHOT

--- arlas-persistence-rest/src/main/java/io/arlas/persistence/rest/PersistenceRestService.java
+++ arlas-persistence-rest/src/main/java/io/arlas/persistence/rest/PersistenceRestService.java
@@ -54,7 +54,7 @@ import java.util.stream.Collectors;
                 title = "ARLAS persistence API",
                 description = "persistence REST services",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "23.0.5"),
+                version = "API_VERSION"),
         schemes = { SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS })
 
 public class PersistenceRestService {
