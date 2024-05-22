5da2e22b74d12885d4fa89a591dbcb80871b0ed5
alainbodiguel <alain.bodiguel@gisaia.com>
2024-05-22 11:57:18 +0200

    Change docker-compose to docker compose

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -12,4 +12,4 @@ function clean_exit {
 trap clean_exit EXIT
 
 echo "===> stop arlas-persistence-server stack"
-docker-compose -f ${DOCKER_COMPOSE} --project-name arlaspersist down -v
+docker compose -f ${DOCKER_COMPOSE} --project-name arlaspersist down -v
