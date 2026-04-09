3511af1f0ddfa78cd0bcfaed8b33a70ea7171ce5
MatthieuBarbet <matthieu.barbet@gisaia.com>
2026-04-09 17:24:35 +0800

    Chore: update arlas-admin to 27.3.2

--- release.sh
+++ release.sh
@@ -179,7 +179,7 @@ fi
 
 echo "=> Start arlas-persistence-server stack"
 docker compose -f ${DOCKER_COMPOSE} --project-name arlaspersist up -d --build
-DOCKER_IP=$(docker-machine ip || echo "localhost")
+DOCKER_IP=$(docker-machine ip 2>/dev/null || echo "localhost")
 
 echo "=> Wait for arlas-persistence-server up and running"
 i=1; until nc -w 2 ${DOCKER_IP} 19997; do if [ $i -lt 30 ]; then sleep 1; else break; fi; i=$(($i + 1)); done
