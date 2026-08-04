bf067336caaa9a5ec6b21e5520f5114054f0b202
MatthieuBarbet <matthieu.barbet@gisaia.com>
2026-08-04 10:24:59 +0800

    Fix: getGroupsByZone response annotation type

--- arlas-persistence-rest/src/main/java/io/arlas/persistence/rest/PersistenceRestService.java
+++ arlas-persistence-rest/src/main/java/io/arlas/persistence/rest/PersistenceRestService.java
@@ -41,6 +41,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -250,7 +251,7 @@ public class PersistenceRestService {
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation",
-                    content = @Content(schema = @Schema(implementation = String[].class))),
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = String.class)))),
             @ApiResponse(responseCode = "404", description = "Zone not found.",
                     content = @Content(schema = @Schema(implementation = Error.class))),
             @ApiResponse(responseCode = "500", description = "Arlas Persistence Error.",
